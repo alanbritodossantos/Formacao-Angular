24daa87ecf2326d2970342d6902cba0f32bb62b0
Alan Brito <alanbritodossantos2015@gmail.com>
2021-03-28 20:19:45 -0300

    ajuste de teste

--- calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -3,20 +3,19 @@ import { TestBed, inject } from '@angular/core/testing';
 import { CalculadoraService } from './calculadora.service';
 
 describe('CalculadoraService', () => {
-  let service: CalculadoraService;
-
+  //let service: CalculadoraService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers:[
         CalculadoraService
       ]
     });
-    service = TestBed.inject(CalculadoraService);
+    //service = TestBed.inject(CalculadoraService);
   });
 
-  it('should be created', () => {
+  it('should...',inject ([CalculadoraService],(service:CalculadoraService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 
   it('deve garantir que 1 + 4 = 5',
     inject([CalculadoraService],(service: CalculadoraService) =>{
