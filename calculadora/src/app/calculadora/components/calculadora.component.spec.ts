7cfc200d225d0c04a17eeadf7bb0fdad7cf54471
Alan Brito <alanbritodossantos2015@gmail.com>
2021-03-29 00:37:59 -0300

    Testando calculadora component

--- calculadora/src/app/calculadora/components/calculadora.component.spec.ts
+++ calculadora/src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async,ComponentFixture, TestBed } from '@angular/core/testing';
+import{ By } from '@angular/platform-browser';
 import { CalculadoraComponent } from './calculadora.component';
 import { CalculadoraService } from '../services';
+import { by } from 'protractor';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -25,4 +27,34 @@ describe('CalculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que 3 + 2 = 5',() => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    //Clique no botão 3
+    btn3.triggerEventHandler('click',null);
+    fixture.detectChanges();//detecta ação
+
+    //clique no botão soma
+    btnSoma.triggerEventHandler('click',null);
+    fixture.detectChanges();
+
+    //clique no botão 2
+    btn2.triggerEventHandler('click',null);
+    fixture.detectChanges();
+
+    //clique no botão '='
+    btnCalcular.triggerEventHandler('click',null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  });
+
+
+
+
 });
